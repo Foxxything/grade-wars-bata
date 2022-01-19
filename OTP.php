2d763f650d0c00bea81ff0cf2e103e544f9d4039
Foxxything <63935304+Foxxything@users.noreply.github.com>
2022-01-19 15:25:20 -0600

    Update OTP.php

--- OTP.php
+++ OTP.php
@@ -1,10 +1,64 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>OTP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+  </head>
+  <body>
+    <div class='align-middle'>
+      <center>
+        <div class="card" style="width: max-content;">
+          <div class="card-header">
+            <h3>OTP Generator</h3>
+          </div>
+          <div class="card-body">
+            <form action="OTP.php" method="post">
+              <div class="form-group">
+                <label for="amount">Amount of OTPs</label>
+                <input type="number" class="form-control" id="amount" name="amount" placeholder="Amount of OTPs">
+              </div>
+              <div class="form-group">
+                <label for="type">Type of OTP</label>
+                <select class="form-control" id="type" name="type">
+                  <option value="1">Teacher</option>
+                  <option value="2">Admin</option>
+                  <option value="3">Both</option>
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary">Generate OTP</button>
+            </form>
+          </div>
+        </div>
+      </center>
+    </div>
+  </body>
+</html>
+
+<?php
+
+  // global variables
+  $otp_array = array();
+
+  $accountTypeKey = [ // used to get name of account type
+    "none" => 0,
+    "teacher" => 1,
+    "admin" => 2,
+    "both" => 3
+  ];
+
   // make the join code
   function makeCode($email, $accountType) {
     $code = rand(100000, 999999); // example: 123456
     $hash = md5($email . $accountType . $code); // example: e10adc3949ba59abbe56e057f20f883e
     $semiFinalCode = array();
- 
+
     for ($i = 0; $i < 6; $i++) {
       // randoom character from the hash
       $semiFinalCode[$i] = $hash[rand(0, strlen($hash) - 1)];
@@ -33,31 +87,22 @@
       echo "\n";
       return explode("|", $decryption);
     }
-
   }
 
-  // if the form is submitted
-  if(true){
-    // get the number of OTP
-    $otp = 70;
-    $preAccountType = 1;
- 
-    $accountTypeKey = [
-      "none" => 0,
-      "teacher" => 1,
-      "admin" => 2,
-      "both" => 3
-    ];
- 
-    // create an array
-    $otp_array = array();
-    // loop through the number of OTP
-    for($i=0; $i<$otp; $i++){
-      // make an dummy email
-      $email = rand(1000000, 9999999) . "@sjasd.ca";
-      $code = makeCode($email, $accountType); // make the join code
+  // Check if the form was submitted and the values are not empty
+  if(isset($_POST) && isset($_POST['amount']) && isset($_POST['type'])) {
+    // Get the values from the form
+    $amount = $_POST['amount'];
+    $preAccountType = $_POST['type'];
+
+    // require the database link from config.php
+    require_once 'config.php';
+
+    for($i=0; $i<$amount; $i++){
+      $email = rand(1000000, 9999999) . "@sjasd.ca"; // make an dummy email (example: 123456@sjasd.ca)
+      $code = makeCode($email, $preAccountType); // make the join code
       $accountType = accountType($preAccountType, $email); // make the account type
-      $decriypedAccountType = accountType($accountType)[1]; // decrypt the account type
+      //$decriypedAccountType = accountType($accountType)[1]; // decrypt the account type
  
       // echo "<br>";
       // echo "+--------------------------------------+<br>";
@@ -69,20 +114,23 @@
       // echo "+--------------------------------------+<br>";
       // echo "<br>";
 
-      $sql = "INSERT INTO `pre_user` (`otp`, `email`, `type`) VALUES " . $code . "," . $email . "," . $accountType;
-      echo $sql."<br>";
-      
-      $finalCode = array(
-        "email" => $email,
-        "code" => $code,
-        "accountType" => array_search($decriypedAccountType, $accountTypeKey)
-      );
+      $sql = "INSERT INTO `pre_user` SET `email` = '$email', `otp` = '$code', `type` = '$accountType'";
+       
+      // run the query and check if it was successful
+      $conn -> query($sql);
+      if($conn -> error) {
+        echo "Error: " . $conn -> error;
+      }
+
+      // $finalCode = array(
+      //   "email" => $email,
+      //   "code" => $code,
+      //   "accountType" => array_search($decriypedAccountType, $accountTypeKey)
+      // );
  
       // push the code to the array
-      array_push($otp_array, $finalCode);
+      // array_push($otp_array, $finalCode);
     }
-    
-    $json = json_encode($otp_array);
-    echo $json;
-}
+
+  }
 ?>
